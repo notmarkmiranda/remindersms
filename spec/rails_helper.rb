b448ff38ab3a9a191169a7dcd82cb9e1658268e6
Mark Miranda <notmarkmiranda@gmail.com>
2020-10-19 14:41:35 -0600

    Add dashboard path, view and request spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,6 +42,9 @@ RSpec.configure do |config|
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
+  # allows for usage of .sign_in(user) in request tests
+  config.include Devise::Test::IntegrationHelpers, type: :request
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
